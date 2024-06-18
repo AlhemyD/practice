d35dbb2776bff65f1e01adee590e44de1f77ed22
mgwoorl <149603747+mgwoorl@users.noreply.github.com>
2024-06-18 18:13:03 +0800

    Function for starting our app was added

--- src/main.py
+++ src/main.py
@@ -1,1 +1,11 @@
+from fastapi import FastAPI
+import uvicorn
 
+app = FastAPI()
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8001)
+
+@app.get("/")
+async def root():
+    return {"message": "всё работает"}
