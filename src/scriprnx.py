e9773ffb446883bb389c3e4a67932acdf62971fe
Deppkepa <149363955+Deppkepa@users.noreply.github.com>
2024-06-18 21:19:30 +0800

    День 1 (Дополнение 1)
    - Не туда скинула файлы.
    - Функция оказалась бесполезной, переделала. Все работает корректно.
    - Связала код для парсинга с своим кодом. Теперь код для парсинга запускается get запросом.

--- src/scriprnx.py
+++ src/scriprnx.py
@@ -1,34 +1,35 @@
 from fastapi import FastAPI
 import os
 from logger import get_logger
+from script1 import parsing
+import subprocess
 
 app = FastAPI()
 logger=get_logger("scriprnx")
 
 def reformat_crx_to_rnx(crx_file_path):
     if not crx_file_path.endswith(".crx"):#Проверяю файл на формат (полезно не удалять!)
-        logger.error(f"The {crx_file_path} does not have the .crx format")
-        return {"Error" : f"Name file '{crx_file_path}' not a .crx file"}
+        logger.error(f"The does not have the .crx format")
+        return {"Error": f"Name file '{crx_file_path}' not a .crx file"}
 
+    rnx_file_path = crx_file_path.replace('.crx', '.rnx')
     file_name_rnx = os.path.basename(os.path.splitext(crx_file_path)[0]) + ".rnx"
     
     if  os.path.exists(file_name_rnx):#Проверка на существование файла с форматом .rnx
         logger.error(f"Such a file already exists. Formatting is not required.")
         return {"Error": "Such a file already exists."}
-    
-    if os.path.exists(crx_file_path):#Проверяю существует ли файл в директории
-        logger.info(f"The beginning of the format change.")
-        rnx_file_path = crx_file_path.replace('.crx', '.rnx')
+
+    if os.path.exists(crx_file_path):
+        try:
+            logger.info(f'{crx_file_path} has been reformatted to {rnx_file_path}')
+            subprocess.run(['/home/dasha/RNXCMP_4.1.0_Linux_x86_32bit/bin/CRX2RNX', crx_file_path, '-f'], check=True)
+            return {"info": "The request has been completed."}
+        except subprocess.CalledProcessError as e:
+            logger.error(f"Error converting CRX to RNX: {e}")
+            return {"error": "Error converting CRX to RNX"}
     else:
         logger.error(f"The file name '{crx_file_path}' does not exist in this directory")
         return {"Error": "The file does not exist."}
-
-    with open(crx_file_path, 'rb') as crx_file, open(rnx_file_path, 'wb') as rnx_file:#преобразовывается содерживое файла в нужный формат
-        rnx_file.write(crx_file.read())
-
-    logger.info(f'{crx_file_path} has been reformatted to {rnx_file_path}')
-
-    return {"info":"The request has been completed."}
 
 @app.get("/")
 def hello():
@@ -39,6 +40,12 @@ def hello():
 async def reformat_crx_by_path(crx_file_path: str):
     logger.info("The file format change has started: start func (reformat_crx_to_rnx)")
     return reformat_crx_to_rnx(crx_file_path)
+
+@app.get("/parsing/{crx_file_path}")
+async def parsing_file_name(crx_file_path: str):
+    logger.info("parsing")
+    return parsing(crx_file_path)
+
 
 if __name__ == "__scriprnx__":
     import uvicorn
