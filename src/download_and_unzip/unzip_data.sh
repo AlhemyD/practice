18200f95dda708e6fbea40d0f3e2159e6269cb2f
mgwoorl <149603747+mgwoorl@users.noreply.github.com>
2024-06-21 02:30:53 +0800

    Unpacking files (crx.gz) and deletion files with another format were added

--- src/download_and_unzip/unzip_data.sh
+++ src/download_and_unzip/unzip_data.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
 
-# unpack the zip archive
-unzip "data/$1.zip"
+# add the data directory at the beginning and .zip at the end
+archive_name="data/$1.zip"
 
+# unpack the file into a folder named after the argument
+unzip "$archive_name" -d "data/$1/"
 
+# remove all files that do not have the crx.gz extension
+for file in $(ls "data/$1" | grep -v "\.crx\.gz"); do
+  rm -f "data/$1/$file"
+done
+
+# unpack files with the crx.gz extension
+for file in $(ls "data/$1" | grep "\.crx\.gz"); do
+  gunzip "data/$1/$file"
+done
