3b3be9f25db0a011613b676ad0f7ab39b160074d
mgwoorl <149603747+mgwoorl@users.noreply.github.com>
2024-06-21 00:03:45 +0800

    Logging was added to file for downloading

--- src/download_and_unzip/download.py
+++ src/download_and_unzip/download.py
@@ -3,16 +3,22 @@ import sys
 from datetime import datetime, timedelta
 import schedule
 import time
+import os
+from src import logger
+
+logger = logger.get_logger("download")
 
 def download_file_from_url():
     today = datetime.now()
     date = (today - timedelta(days=200)).strftime('%Y-%m-%d')
-    #link = f"https://api.simurg.space/datafiles/map_files?date={date}"
     #link = "https://bki.matecdn.ru/-/10e7e137-4fc3-45d1-a37e-bdd4a390e537/darw00aus-r-20240010000-01d-30s-mocrx.gz"
     link = f"https://api.simurg.space/datafiles/map_files?date={date}"
     file_name = f"{date}.zip"
 
+    logger.info(f"Downloading file from {link}...")
+
     with open(file_name, "wb") as f:
+        print("Downloading %s" % file_name)
         response = requests.get(link, stream=True)
         total_length = response.headers.get('content-length')
 
@@ -28,7 +34,5 @@ def download_file_from_url():
                 sys.stdout.write("\r[%s%s]" % ('=' * done, ' ' * (50 - done)))
                 sys.stdout.flush()
 
-schedule.every().day.at("20:39").do(download_file_from_url)
-
-while True:
-    schedule.run_pending()
+    print(f"{file_name} was downloaded successfully")
+    logger.info(f"File downloaded successfully to {file_name}")
