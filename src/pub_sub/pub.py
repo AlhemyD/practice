6de3de20ed0f104cf3eebe0bbb983cecba109269
AlhemyD <AlhemyD@mail.ru>
2024-06-24 13:16:46 +0000

    Дорабатываю pub.py. Теперь файл принимает в качестве аргумента имя rnx файла и парсит его с помощью запроса на FastApi, при условии, что fastapi запущен

--- src/pub_sub/pub.py
+++ src/pub_sub/pub.py
@@ -1,32 +1,46 @@
 import paho.mqtt.client as mqtt_client
 import sys
-sys.path.append("../models")
+sys.path.append("../modules")
 from models import Publisher
+sys.path.append("../log")
+from logger import get_logger
+import requests
 
-
+logger = get_logger("pub")
 '''
 скрипт вызывается командой вида^
 
-python3 pub.py {station} {arg1 arg2 arg3}
+python3 pub.py {rnx_file_name}
 
 т.е.
 
-python3 pub.py station1 1 2 3 4 5 6
+python3 pub.py darw00aus-r-20240010000-01d-30s-mo.rnx
 
 
 
 '''
-station = sys.argv[1] #Тогда здесь будет station1
-data = sys.argv[2:] #А здесь будет список ['1', '2', '3', '4', '5', '6']
+rnx_file_name = sys.argv[1] #Тогда здесь будет имя файла rnx
+#data = sys.argv[2:] #А здесь будет список ['1', '2', '3', '4', '5', '6']
 
 '''
 Предполагается, что в data будут передаваться пропарсенные данные, 
 которые нужно опубликовать
 '''
-pub = Publisher(station,"localhost","station/data")
-pub.connect()
-pub.loop_start()
-for i in data:
-    pub.publish(f"{pub.station}@%@%!{i}")
-pub.disconnect()
-pub.loop_stop()
+pub = Publisher(rnx_file_name,"localhost","station/data")
+#for i in data:
+#    pub.publish(f"{pub.station}@%@%!{i}")
+
+
+    
+#date = (today - timedelta(days=200)).strftime('%Y-%m-%d')
+date="2024-06-23"
+link=f"http://localhost:8000/parsing/{date}/{rnx_file_name}"
+response = requests.get(link, stream=True)
+#print(response.parse_data[0])
+def publ(pub, data):
+    pub.connect()
+    pub.loop_start()
+    pub.publish(f"{pub.station}@%@%!{data}")
+    pub.disconnect()
+    pub.loop_stop()
+publ(pub, rnx_file_name)
