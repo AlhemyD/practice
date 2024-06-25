2ab3cbb27cf4141aca3ccd59fdb53def45eccbfb
AlhemyD <AlhemyD@mail.ru>
2024-06-25 16:34:29 +0000

    Correcting mistakes x 3. Added additional paths to scriprnx

--- src/question/scriprnx.py
+++ src/question/scriprnx.py
@@ -18,7 +18,8 @@ def hello():
 @app.get("/reformat/{der}/{file_name}")
 async def reformat_crx_by_path(der:str, file_name: str):
     path="../../data"
-    crx_file_path = os.path.join(os.path.dirname(os.path.realpath(__file__)),path, der, file_name)
+    crx_file_path = os.path.join(path, der, file_name)
+    crx_file_path=os.path.join(os.path.dirname(os.path.realpath(__file__)),crx_file_path)
     logger.info("The file format change has started: start func (reformat_crx_to_rnx)")
     return reformat_crx_to_rnx(crx_file_path)
 
