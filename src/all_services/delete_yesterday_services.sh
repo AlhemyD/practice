81e4d1e19cbbf377c3ee8c0d826e1bb4ba3f7b72
mgwoorl <149603747+mgwoorl@users.noreply.github.com>
2024-06-27 23:48:55 +0800

    added deletion of files from the previous day, so as not to fill up memory

--- src/all_services/delete_yesterday_services.sh
+++ src/all_services/delete_yesterday_services.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+user=$(logname)
+
 if [ $# -eq 0 ]; then
     echo "Please provide a date as an argument."
     exit 1
@@ -18,4 +20,18 @@ else
     systemctl daemon-reload
 
     echo "All unit files starting with 'station_@' and ending with '$_.service' have been deleted from /etc/systemd/system/ directory."
+fi
+
+stations_dir="/home/$user/practice/data/$1"
+
+if [[ ! -e "$stations_dir" ]]; then
+    echo "This directory $stations_dir doesn't exist."
+else
+    if [ -z "$(ls -A "$stations_dir")" ]; then
+        echo "The directory $stations_dir is empty."
+    else
+        rm -f "$stations_dir"/*
+        rmdir "$stations_dir"
+        echo "All files and the folder $stations_dir have been deleted."
+    fi
 fi
