9585326227ab174ebd50d1dc9e9343e47555842b
AlhemyD <138218446+AlhemyD@users.noreply.github.com>
2024-06-28 05:34:43 +0800

    Сшивая проект

--- src/all_services/create_pub_services.sh
+++ src/all_services/create_pub_services.sh
@@ -2,9 +2,16 @@
 
 user=$(logname)
 
-data_dir="/home/$user/practice/data/$1"
+if [ -d "/home/$user/practice/data" ]; then
+    pattern="/home/$user"
+else
+    pattern="/home/$user/src"
+fi
 
-path_to_pub="/home/$user/practice/src/pub_sub/pub.py"
+data_dir="$pattern/practice/data/$1"
+
+
+path_to_pub="$pattern/practice/src/pub_sub/pub.py"
 
 # Перебираем файлы по дате в директории data
 for file in $(ls -t $data_dir); do
@@ -23,7 +30,7 @@ for file in $(ls -t $data_dir); do
         fi
     else
         # Создаем новый юнит файл для данного файла
-        cp /home/$user/practice/src/all_services/station_@.service /etc/systemd/system/$unit_name
+        cp $pattern/practice/src/all_services/station_@.service /etc/systemd/system/$unit_name
         
 	if [[ $file == *rnx ]]; then
             sed -i "s/%i/${file%%_*}/g" /etc/systemd/system/$unit_name
@@ -31,7 +38,7 @@ for file in $(ls -t $data_dir); do
             sed -i "s/%i/${file:0:4}/g" /etc/systemd/system/$unit_name
         fi
 
-	sed -i "s|%pub_place|${path_to_pub}|g" /etc/systemd/system/$unit_name
+	sed -i "s@%file_path@$path_to_pub@g" /etc/systemd/system/$unit_name
         sed -i "s/%date/$1/g" /etc/systemd/system/$unit_name
         sed -i "s/%user/$user/g" /etc/systemd/system/$unit_name
         systemctl daemon-reload
--- src/all_services/create_scriprnx_service.sh
+++ src/all_services/create_scriprnx_service.sh
@@ -15,10 +15,14 @@ fi
 if [[ -f /etc/systemd/system/$unit_name ]]; then
     if ! systemctl is-active --quiet $unit_name; then
         systemctl start $unit_name
+	echo "NOELZE"
     fi
 
 else
     cp $pattern/src/all_services/fastapi_@.service /etc/systemd/system/$unit_name
+    scrippath="$pattern/src/question/scriprnx.py"
+    echo "ELSE"
+    sed -i "s@%scrippath@$scrippath@g" /etc/systemd/system/$unit_name
     sed -i "s/%user/$user/g" /etc/systemd/system/$unit_name
     systemctl daemon-reload
     systemctl enable $unit_name
