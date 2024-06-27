ad612941bdfb162fd09dc77c3ab39e46322399b1
mgwoorl <149603747+mgwoorl@users.noreply.github.com>
2024-06-28 03:21:10 +0800

    Fixed errors that occurred during generation

--- src/all_services/create_pub_services.sh
+++ src/all_services/create_pub_services.sh
@@ -2,16 +2,9 @@
 
 user=$(logname)
 
-if [ -d "/home/$user/practice/data" ]; then
-    pattern="/home/$user"
-else
-    pattern="/home/$user/src"
-fi
+data_dir="/home/$user/practice/data/$1"
 
-data_dir="$pattern/practice/data/$1"
-
-
-path_to_pub="$pattern/practice/pub_sub_pub.py"
+path_to_pub="/home/$user/practice/src/pub_sub/pub.py"
 
 # Перебираем файлы по дате в директории data
 for file in $(ls -t $data_dir); do
@@ -30,7 +23,7 @@ for file in $(ls -t $data_dir); do
         fi
     else
         # Создаем новый юнит файл для данного файла
-        cp $pattern/practice/src/all_services/station_@.service /etc/systemd/system/$unit_name
+        cp /home/$user/practice/src/all_services/station_@.service /etc/systemd/system/$unit_name
         
 	if [[ $file == *rnx ]]; then
             sed -i "s/%i/${file%%_*}/g" /etc/systemd/system/$unit_name
@@ -38,7 +31,7 @@ for file in $(ls -t $data_dir); do
             sed -i "s/%i/${file:0:4}/g" /etc/systemd/system/$unit_name
         fi
 
-	sed -i "s/%file_path/$path_to_pub/g" /etc/systemd/system/$unit_name
+	sed -i "s|%pub_place|${path_to_pub}|g" /etc/systemd/system/$unit_name
         sed -i "s/%date/$1/g" /etc/systemd/system/$unit_name
         sed -i "s/%user/$user/g" /etc/systemd/system/$unit_name
         systemctl daemon-reload
